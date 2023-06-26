9b1418b77c37f783f093ae4bf0a836f613be3e1e
Denitsu <notdenitsu@gmail.com>
2023-06-26 20:44:37 +0300

    Changed page titles

--- GalleryGaze/frontend/home.php
+++ GalleryGaze/frontend/home.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
     <link rel="stylesheet" href="../static/css/normalize.css">
     <link rel="stylesheet" href="../static/css/style.css">
     <link rel="stylesheet" href="../static/assets/icons/fontawesome/css/all.min.css">
